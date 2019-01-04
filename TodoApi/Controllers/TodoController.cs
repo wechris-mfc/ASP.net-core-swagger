35f004824890cf853c392dca3ddc650fad7a3a5c
Jason Williams <jasonwilliams2308@gmail.com>
2019-01-04 19:03:49 +0000

    Add project files.

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (!_context.TodoItems.Any())
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+
+        /// <summary>
+        /// Gets All TodoItems.
+        /// </summary>
+        /// <returns>A List of todo Items </returns>
+        /// <response code="200">Returns TodoItems as Json Object </response>
+        /// <response code="400">If the item is null</response>  
+        // GET: api/Todo
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+
+        /// <summary>
+        /// Get a single TodoItem.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>get a single TodoItem</returns>
+        /// <response code="200">Returns the todoItem as json</response>
+        /// <response code="404">If the item is null</response>
+        // GET: api/Todo/1 
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound("Item not found");
+            }
+
+            return todoItem;
+        }
+
+
+
+        /// <summary>
+        /// Creates a TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="todoItem"></param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>            
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            await _context.TodoItems.AddAsync(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+
+        }
+
+        ///  <summary>
+        ///  Updates a todoItem.
+        ///  </summary>
+        ///  <remarks>
+        ///  Sample request:
+        /// 
+        ///      PUT /Todo/1
+        ///      {
+        ///         "id": 1,
+        ///         "name": "Item1",
+        ///         "isComplete": true
+        ///      }
+        /// 
+        ///  </remarks>
+        /// <param name="id"></param>
+        /// <param name="todoItem"></param>
+        ///  <returns>A newly created TodoItem</returns>
+        ///  <response code="204">Success - No Content Returned</response>
+        ///  <response code="404">If the item is not found</response>            
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        ///  <summary>
+        ///  Deletes a todoItem.
+        ///  </summary>
+        /// <param name="id"></param>
+        ///  <returns>A newly created TodoItem</returns>
+        ///  <response code="200">Success - Deleted json object</response>
+        ///  <response code="404">If the item is not found</response>  
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return todoItem;
+        }
+    }
+}
---
+++ TodoApi/Startup.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register Swagger Generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "My API",
+                    Description = "API Example ASP.NetCore 2.1",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Jason Williams",
+                        Email = "jasonwilliams@letuscode.co.uk",
+                        Url = "http://letuscode.co.uk"
+                    },
+                    License = new License
+                    {
+                        Name = "open source",
+                        Url = "http://example.com"
+
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+
+
+            // Enable Swagger middleware and endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
